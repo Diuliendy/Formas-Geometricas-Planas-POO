ea5f293b53f7be76d95ea60a5183f2a15455c14b
Diuliendy <141693330+Diuliendy@users.noreply.github.com>
2025-06-02 23:22:04 -0300

    Update Losango.java

--- Formas-Geometricas-Planas-Encapsulamento/Losango.java
+++ Formas-Geometricas-Planas-Encapsulamento/Losango.java
@@ -1,24 +1,37 @@
 package br.edu.figurasgeometricasplanas;
 import java.util.Scanner;
 public class Losango {
-	double diagonalhorizontal;
-	double diagonalvertical;
-	
-	
-public Losango(double diagonalhorizontal, double diagonalvertical) {
-	Scanner sc= new Scanner(System.in);
-	
-	diagonalhorizontal=sc.nextDouble();
-	diagonalvertical=sc.nextDouble();
-}
+	public class Losangulo {
+	    private double maior;
+	    private double menor;
+	    private double area;
 
-public double calcularArea() {
-	return(diagonalhorizontal*diagonalvertical)/2;
-}
+	    public void calcArea() {
+	        this.area = (maior * menor) / 2;
+	    }
 
-public double listadeAtributos() {
-	return 2;
-}
+	    public void recebeMaior(double maior) {
+	        this.maior = maior;
+	    }
 
+	    public void recebeMenor(double menor) {
+	        this.menor = menor;
+	    }
 
+	    public double mostraAreaComRetorno() {
+	        return this.area;
+	    }
+
+	    public double getMaior() {
+	        return maior;
+	    }
+
+	    public double getMenor() {
+	        return menor;
+	    }
+
+	    public double getArea() {
+	        return area;
+	    }
+	}
 }
